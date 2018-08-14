6f501c79eafc36ddf2376114f6ba60d2a6a705bd
mgmolisani <molisani.m@husky.neu.edu>
2018-08-14 10:47:40 -0400

    Was set to local host. Updating for deploy.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(session({
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        ["http://localhost:4200"]);
+        ["https://mmolisani-final-project-node.herokuapp.com/api/events"]);
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
