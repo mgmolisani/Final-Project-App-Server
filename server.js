dcaae4d56ca984c917a42169689465ec910ab7c9
mgmolisani <molisani.m@husky.neu.edu>
2018-08-18 15:11:48 -0400

    final with correct url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(session({
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        ["https://mmolisani-final-project-node.herokuapp.com"]);
+        ["https://mmolisani-final-project-cs5610.herokuapp.com"]);
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
